e78b5b0463da68dbce61f2c473bcccabd5baec3c
cacook7 <cacook7@crimson.ua.edu>
2021-10-28 15:13:16 -0500

    update

--- Database/EditPost.cs
+++ Database/EditPost.cs
@@ -30,32 +30,6 @@ namespace spring2021_pa3_cacook7.Database
 
             cmd.ExecuteNonQuery();
 
-
-
-
-            // ConnectionString myConnection = new ConnectionString();
-            // string cs = myConnection.cs;
-            // using var con = new MySqlConnection(cs);
-            // con.Open();
-
-            // Console.WriteLine("What is the id of the post you want to edit?");
-            // int id = int.Parse(Console.ReadLine());
-
-            // string stm = $@"UPDATE posts(text, date) VALUES(@text, @date) WHERE id = @id";
-
-            // using var cmd = new MySqlCommand(stm, con);
-
-            // Console.WriteLine("What would you like to update your post to say?");
-            
-            // string Text = Console.ReadLine();
-            // DateTime Date = DateTime.Now;
-
-            // cmd.Parameters.AddWithValue("@text", Text);
-            // cmd.Parameters.AddWithValue("@date", Date);
-
-            // cmd.Prepare();
-
-            // cmd.ExecuteNonQuery();
         }
     }
 }
